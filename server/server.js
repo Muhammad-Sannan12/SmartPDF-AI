0f2e96dfa0ccc2548ab8e0866aa0af72dda65916
Muhammad Sannan Sherzada <24pwbcs1177@uetpeshawar.edu.pk>
2026-04-16 00:48:03 +0500

    backend ready for deploy

--- server/server.js
+++ server/server.js
@@ -10,8 +10,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/files", fileRoutes);
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
